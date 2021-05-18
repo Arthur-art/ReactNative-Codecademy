2ee0c817d5b4fdac45ce1f77a123c390a57b8526
Arthur-art <arthurteixeira.guts@gmail.com>
2021-05-18 17:12:15 -0300

    Utilizando Text component do react native

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View } from 'react-native'
-import { FlexBox } from './src/components/FlexBox'
+import { TextComponent } from './src/components/TextComponent'
 // O componente View é melhor comparável a um div elemento HTML. Da mesma forma div, 
 // o Viewcomponente não é visível a menos que um estilo seja aplicado.
 //  Podemos aplicar esse estilo por meio do style props em camelCase.
 
 export default function App() {
   return (
-    <FlexBox />
+    <TextComponent />
   );
 }
