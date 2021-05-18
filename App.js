41fbf78258cc153797c449154401ff32ec536eb4
Arthur-art <arthurteixeira.guts@gmail.com>
2021-05-18 17:25:27 -0300

    Organizando components em pastas separadas

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { View } from 'react-native'
 import { TextComponent } from './src/components/TextComponent'
-// O componente View é melhor comparável a um div elemento HTML. Da mesma forma div, 
-// o Viewcomponente não é visível a menos que um estilo seja aplicado.
-//  Podemos aplicar esse estilo por meio do style props em camelCase.
 
 export default function App() {
   return (
--- src/components/FlexBox/index.js
+++ src/components/FlexBox/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { View } from 'react-native'
+// O componente View é melhor comparável a um div elemento HTML. Da mesma forma div, 
+// o Viewcomponente não é visível a menos que um estilo seja aplicado.
+//  Podemos aplicar esse estilo por meio do style props em camelCase.
+
 
 export function FlexBox() {
 
