a87ce9eef5546b25ddc1ef1e5e9ecbdc139d67a1
Arthur-art <arthurteixeira.guts@gmail.com>
2021-05-18 17:49:50 -0300

    Trabalhando com imagens usando component Image do react native

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { View } from 'react-native'
-import { TextComponent } from './src/components/TextComponent'
+import { View, Image } from 'react-native';
+const image = require('./assets/favicon.png')
 
 export default function App() {
   return (
-    <TextComponent />
+
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Image source={{ uri: 'https://picsum.photos/100/100' }} style={{ width: 100, height: 100 }} />
+      <Image style={{ width: 100, height: 100, }} source={image} />
+    </View>
   );
 }
