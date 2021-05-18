26157bf765ac2fe4959a8cfad93fe1bcfa125d08
Arthur-art <arthurteixeira.guts@gmail.com>
2021-05-18 17:21:05 -0300

    Estilizando Text component do react native

--- src/components/TextComponent.js
+++ src/components/TextComponent.js
@@ -4,8 +4,9 @@ import { View, Text } from 'react-native'
 export function TextComponent() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>
-                Hello World
+            <Text style={{ fontSize: 14 }}>
+                Aprendendo React Native na
+                 <Text style={{ fontSize: 20, fontWeight: 'bold' }}> Codecademy</Text>
             </Text>
         </View>
     )
