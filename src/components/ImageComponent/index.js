b5293c422d51d64b1cd935b2cea06b6beacf7a87
Arthur-art <arthurteixeira.guts@gmail.com>
2021-05-19 10:13:39 -0300

    Praticando flex box react native

--- src/components/ImageComponent/index.js
+++ src/components/ImageComponent/index.js
@@ -5,11 +5,15 @@ const image = require('../../../assets/dngtransportes-logo.jpg')
 export const ImageComponent = () => {
     return (
 
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#bdc3c7' }}>
-            <Image style={{ width: 100, height: 100, }} source={image} />
+        <View style={{
+            flex: 1, justifyContent: "space-around", flexDirection: "row",
+            alignItems: 'center', backgroundColor: '#bdc3c7',
+        }}>
+            <Image style={{ width: 100, height: 100, borderRadius: 20 }} source={image} />
             <Image source={{ uri: 'https://avatars.githubusercontent.com/u/54858003?v=4' }}
                 style={{ width: 100, height: 100, borderRadius: 20 }} />
         </View>
+
     )
 }
 
